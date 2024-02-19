f1599ed31721b090418b013052d95ea7bd6b1d5c
Stanislao <aquilante93@gmail.com>
2024-02-19 19:28:34 +0100

    WIP

---
+++ src/main/java/com/slfl/portfolio_project/service/AuthenticationServiceWrapper.java
@@ -0,0 +1,44 @@
+package com.slfl.portfolio_project.service;
+
+import java.util.*;
+
+import com.slfl.portfolio_project.model.*;
+import com.slfl.portfolio_project.repository.RoleRepository;
+import com.slfl.portfolio_project.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@Slf4j
+public class AuthenticationServiceWrapper {
+
+    private AuthenticationService authenticationService;
+    public ResponseStatus registerUser(String username, String email, String password, boolean isPhotographer) {
+        return authenticationService.registerUser(username,email,password,isPhotographer);
+    }
+
+    public ResponseStatus loginUser(String username, String password) {
+        ResponseStatus responseStatus = authenticationService.loginUser(username,password);
+        if(responseStatus.getCode() == "200"){
+            log.info("L'utente " + username + "ha effettuato correttamente l'accesso");
+        }
+        return responseStatus;
+    }
+
+    public ResponseStatus checkExistUser(String username, String email) {
+        return authenticationService.checkExistUser(username, email);
+    }
+
+    public ResponseStatus checkExistUserLogin(String username) {
+        return authenticationService.checkExistUserLogin(username);
+    }
+}

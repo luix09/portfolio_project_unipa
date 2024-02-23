4890923a9e9d36185dcf5c0c262981e9f8c6db0e
Lorenzo Virano <lorenzo.virano@meetweb.it>
2024-02-23 11:57:50 +0100

    Fix response endpoint home/photographer/:photographer

--- src/main/java/com/slfl/portfolio_project/model/response_factory/user/ListPhotographersResponse.java
+++ src/main/java/com/slfl/portfolio_project/model/response_factory/user/ListPhotographersResponse.java
@@ -2,10 +2,12 @@ package com.slfl.portfolio_project.model.response_factory.user;
 
 import com.slfl.portfolio_project.model.response_factory.CustomResponse;
 import com.slfl.portfolio_project.model.user.Photographer;
+import com.slfl.portfolio_project.model.user.User;
+
 import java.util.List;
 
 public class ListPhotographersResponse extends CustomResponse {
-    public ListPhotographersResponse(String code, String message, List<Photographer> data) {
+    public ListPhotographersResponse(String code, String message, List<User> data) {
         super(code, message, data);
     }
 }
--- src/main/java/com/slfl/portfolio_project/model/response_factory/user/UserResponseFactory.java
+++ src/main/java/com/slfl/portfolio_project/model/response_factory/user/UserResponseFactory.java
@@ -3,19 +3,20 @@ package com.slfl.portfolio_project.model.response_factory.user;
 import com.slfl.portfolio_project.model.user.Photographer;
 import com.slfl.portfolio_project.model.response_factory.CustomResponse;
 import com.slfl.portfolio_project.model.response_factory.ResponseFactory;
+import com.slfl.portfolio_project.model.user.User;
 
 import java.util.List;
 
 public class UserResponseFactory extends ResponseFactory {
     @Override
     public CustomResponse createDataResponse(Object data) {
-        return new PhotographerInfoResponse("200", "Informazioni del fotografo ottenute con successo.", (Photographer) data);
+        return new PhotographerInfoResponse("200", "Informazioni del fotografo ottenute con successo.", (User) data);
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public CustomResponse createListDataResponse(Object data) {
-        return new ListPhotographersResponse("200", "Lista di fotografi ritornata con successo.", (List<Photographer>) data);
+        return new ListPhotographersResponse("200", "Lista di fotografi ritornata con successo.", (List<User>) data);
     }
 
     @Override
